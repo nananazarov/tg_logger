404d2688fea047d96fa3b276c4eda82d2fd327dd
nananazarov <nazarovnikita97@gmail.com>
2025-03-21 00:23:22 +0700

    Add initial project setup with database configuration and migration support

---
+++ config.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+env_path = Path(__file__).resolve().parent
+
+
+class Config(BaseSettings):
+    host: str = Field(..., alias="POSTGRES_HOST")
+    port: int = Field(..., alias="POSTGRES_PORT")
+    user: str = Field(..., alias="POSTGRES_USER")
+    password: str = Field(..., alias="POSTGRES_PASSWORD")
+    db_name: str = Field(..., alias="POSTGRES_DB")
+
+    @property
+    def uri(self) -> str:
+        return f"postgresql+asyncpg://{self.user}:{self.password}@{self.host}:{self.port}/{self.db_name}"
+
+    model_config = SettingsConfigDict(env_file=(env_path / ".env", env_path / ".env.local"))
+
+
+config = Config()
